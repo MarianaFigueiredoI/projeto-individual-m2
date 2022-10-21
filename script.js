4abf4fa60f3ebc7c46d3f589b1b732b63fb8b29b
MarianaFigueiredoI <113534989+MarianaFigueiredoI@users.noreply.github.com>
2022-10-20 21:19:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+var codificar = document.querySelector("#codificar");
+var decodificar = document.querySelector("#decodificar");
+var incremento = document.querySelector(".chave");
+var selecione = document.querySelector(".select");
+var btnRadio = document.querySelector(".radio-button");
+var btn = document.querySelector("#btnCod");
+var resultado = document.querySelector("#result");
+
+// Função da Base64.
+function base64() {
+  var mensagem = document.querySelector("#msg").value;
+  if (codificar.checked) {
+    var codificado = btoa(mensagem);
+    return codificado;
+  } 
+  else if (decodificar.checked) {
+    var decodificado = atob(mensagem);
+    return decodificado;
+  }
+}
+
+// Incremento utilizado na Cifra de César.
+selecione.addEventListener("click", function () {
+  if (selecione.value == "cesar") {
+    incremento.style.display = "block";
+  } 
+  else {
+    incremento.style.display = "none";
+  }
+})
+
+// Função da Cifra de César.
+function cifraCesar() {
+  var msg = document.querySelector("#msg").value;
+  var chave = parseInt(document.querySelector("#rangenumber").value);
+  var saida = '';
+  
+  if (codificar.checked) {
+    for (let i = 0; i < msg.length; i++) {
+      if (msg[i] === msg[i].toUpperCase()) {
+        saida += String.fromCharCode((msg.charCodeAt(i) + chave - 65) % 26 + 65); 
+    } 
+      else {
+        saida += String.fromCharCode((msg.charCodeAt(i) + chave - 97) % 26 + 97);
+      }
+    }   
+    return saida;
+  } 
+  else if (decodificar.checked) {
+    for (let i = 0; i < msg.length; i++) {
+      if (msg.charCodeAt(i) >= 97 && msg.charCodeAt(i) <= 122) {
+        saida += String.fromCharCode((msg.charCodeAt(i) - 97 -  chave + 26) % 26 + 97);
+      } 
+      else if (msg.charCodeAt(i) >= 65 && msg.charCodeAt(i) <= 90) {
+        saida += String.fromCharCode((msg.charCodeAt(i) - 65 - chave + 26) % 26 + 65);
+      } 
+      else {
+        saida += String.fromCharCode(msg.charCodeAt(i));
+      }
+    }
+    return saida;
+  }
+}
+
+// Alteração de texto do botão.
+btnRadio.addEventListener("click", function () {
+  if (codificar.checked) {
+    btn.innerHTML = "Codificar Mensagem!";
+  } 
+  else if (decodificar.checked) {
+    btn.innerHTML = "Decodificar Mensagem!";
+  }
+})
+
+// Funcionamento do botão de acordo com o método de codificação selecionado.
+btn.addEventListener("click", function (event) {
+  event.preventDefault();
+  if (selecione.value === "base64") {
+    resultado.innerText = base64();
+  } 
+  else if (selecione.value === "cesar") {
+      resultado.innerText = cifraCesar();
+  }
+})
